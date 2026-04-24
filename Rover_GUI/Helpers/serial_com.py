93a50f68209d895a9ee2bdea1f02c5c08feea8a6
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-04-24 21:59:36 +0530

    Added Class motor and direction methods to serial_comm helper

--- Rover_GUI/Helpers/serial_com.py
+++ Rover_GUI/Helpers/serial_com.py
@@ -1,1 +1,36 @@
 # Communicates with the Arduino over USB port
+"""
+Controls:
+    - Left Motor: 0 to 255
+    - Right Motor: 0 to 255
+    - Gimbal Pitch: 0 to 180 degrees
+    - Gimbal Yaw: 0 to 180 degrees
+    - Light: 0 to 255 (brightness)
+"""
+
+
+
+
+
+class motor():
+    def forward(self,speed_threshold):
+        return None
+    
+    def backward(self, speed):
+        return None
+    
+    def left(self,speed):
+        return None
+    
+    def right(self, speed):
+        return None
+    
+    def halt(self):
+        return None
+    
+    def glide_left(self):
+        return None
+    
+    def glide_right(self):
+        return None
+        
